94f23597e277f0d137556684bd57ac7e97f39db5
LucasFox <lucass@lagash.com>
2018-08-08 17:41:25 -0300

    Finalizado Tree y breadcrumbs

--- Documents/Controllers/HomeController.cs
+++ Documents/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ namespace Documents.Controllers
 
             model.CurrentFolder = this._folderService.getCurrentFoler(ConfigurationManager.AppSettings["DocumentsPath"]);
 
+
             if (model.CurrentFolder.MainDocument != null)
             {
                 var mark = new Markdown();
@@ -45,40 +46,55 @@ namespace Documents.Controllers
             return View("Directory", model);
         }
 
-        public ActionResult Directory(string path)
+        // GET: Prueba
+        public ActionResult Test(string path)
+        {
+            if(!Path.HasExtension(path)){
+                return Json(this._folderService.getCurrentFoler(path), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("");
+            }
+            
+            
+        }
+
+        public ActionResult Directory()
         {
             var model = new PageModel();
 
-            model.CurrentFolder = this._folderService.getCurrentFoler(path);
-            if (model.CurrentFolder.MainDocument != null)
-            {
-                var mark = new Markdown();
-                var file = System.IO.File.ReadAllText(model.CurrentFolder.MainDocument.Path);
-                //var html = CommonMark.CommonMarkConverter.Convert(file);
-                var html = MarkDownHelper.Convert(file, "markdown", "html5");
 
-                IHtmlString str = new HtmlString(html);
-                var PageModel = new PageModel();
-                model.Content = str;
-            }
-
-            return View("Directory", model);
+            return View("Directory");
         }
+
+
         public ActionResult ViewDoc(string path)
         {
-
             var mark = new Markdown();
             var file = System.IO.File.ReadAllText(path);
             //var html = CommonMark.CommonMarkConverter.Convert(file);
             var html = MarkDownHelper.Convert(file, "markdown", "html5");
 
             IHtmlString str = new HtmlString(html);
-            var PageModel = new PageModel();
-            PageModel.Content = str;
-            PageModel.CurrentFolder = this._folderService.getCurrentFoler(Path.GetDirectoryName(path));
 
-            return View("ViewDoc", PageModel);
+            return Content(str.ToString());
         }
+
+
+        public FileResult DownloadDoc(string path)
+        {
+            var mark = new Markdown();
+            var file = System.IO.File.ReadAllText(path);
+            var html = MarkDownHelper.Convert(file, "markdown", "html5");
+            var ads = MarkDownHelper.Convert(html, "html5", "txt");
+            Console.WriteLine("asd");
+
+            IHtmlString str = new HtmlString(html);
+            return File(str.ToString(), "application/pdf", "DownloadName.pdf");
+            ;
+        }
+
 
         public ActionResult About()
         {
