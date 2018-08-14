6a02ae8d3035561d48a8751875a02f4b434c8d8d
LucasFox <lucass@lagash.com>
2018-08-14 11:49:36 -0300

    Change icon by event

---
+++ Documents/Content/theme/js/iconos.js
@@ -0,0 +1,7 @@
+﻿
+
+document.getElementById('myClass').addEventListener('click', function () {
+    var icon = document.getElementById('myClass');
+    icon.classList.toggle('fa-angle-right');
+    icon.classList.toggle('fa-angle-down');
+})
